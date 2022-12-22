f6a6fe79cdfc1bfd5398a05b22d68cb2c44b0ee8
stacyy713 <71102900+stacyy713@users.noreply.github.com>
2022-12-22 13:40:41 +0200

    Add files via upload

---
+++ WebprojectApplication.java
@@ -0,0 +1,12 @@
+package com.cleaning.webproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebprojectApplication.class, args);
+	}
+}
