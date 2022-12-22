79af010f380364eca564d67a5508e8c4008af4c7
stacyy713 <71102900+stacyy713@users.noreply.github.com>
2022-12-22 13:43:22 +0200

    Add files via upload

---
+++ config/SecuringWebApplication.java
@@ -0,0 +1,12 @@
+package com.cleaning.webproject.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebApplication {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(SecuringWebApplication.class, args);
+    }
+}
